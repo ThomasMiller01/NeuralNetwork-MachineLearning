830ea30957ecb08febc1ddf55bbb6a1e46fe47f3
Thomas Miller <thomas.miller01.mail@gmail.com>
2019-07-14 12:31:54 +0200

    removed train method

--- neural-network-lib/neuralnetwork.py
+++ neural-network-lib/neuralnetwork.py
@@ -14,15 +14,6 @@ class NeuralNetwork:
             a = self.activation(np.matmul(w, a) + b)
         return a
 
-    def train(self, images, labels):
-        predictions = self.predict(images)
-        labeled_predictions = [(np.argmax(prediction), np.argmax(label))
-                               for prediction, label in zip(predictions, labels)]
-        error = [(desired - guess) * .01 for guess,
-                 desired in labeled_predictions]
-        new_weights = [error + w for w in self.weights]
-        print(error)
-
     def get_accuracy(self, images, labels):
         predictions = self.predict(images)
         num_correct = sum([np.argmax(prediction) == np.argmax(label)
